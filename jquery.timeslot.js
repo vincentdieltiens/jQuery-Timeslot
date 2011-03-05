a40774819b92b82a9966d8b7ec1213e3a3f0b1bf
Vincent Dieltiens <and1hotsauce@gmail.com>
2011-03-05 13:11:08 +0100

    background for the timeline

--- jquery.timeslot.js
+++ jquery.timeslot.js
@@ -4,31 +4,38 @@
     
     return this.each(function(){
       var $timeline = $(this);
-      var timeline = new Timeline($timeline);
+      var timeline = new Timeline($timeline, options);
       timeline.build_timeline();
     });
   };
   
   $.fn.timeslot.defaults = {
-    
+    labelClass: 'label',
+    hourClass: 'hour',
+    quarterClass: 'quarter',
+    maxNumberSlot: 3
   };
   
-  function Timeline($timeline)
+  function Timeline($timeline, options)
   {
     this.$timeline = $timeline;
+    this.options = options;
   };
   
   Timeline.prototype = {
+    /**
+     * Build the html code of the timeline
+     */
     build_timeline: function() {
       
       for(var h=0; h < 24; h++) {
-        $label_div= $('<div />')
+        $label_div= $('<div />').addClass(this.options.labelClass)
         if( h > 0 ) {
-          $label_div.html(h).addClass('label');;
+          $label_div.html(h);
         } else {
-          $label_div.html('&nbsp;').addClass('label0');;
+          $label_div.html('&nbsp;');
         }
-        this.$timeline.append($label_div)
+        this.$timeline.append($label_div);
       }
       
       var $hr = $('<hr />').css({
@@ -41,10 +48,11 @@
       this.$timeline.append($hr);
       
       for(var h=0; h < 24; h++) {
-        $hour_div = $('<div />').addClass('hour');
+        $hour_div = $('<div />').addClass(this.options.hourClass);
         
         for(var q=0; q < 4; q++) {
-          $quarter_div = $('<div />').addClass('quarter');
+          $quarter_div = $('<div />').addClass(this.options.quarterClass).html($('<div />'));
+          
           $hour_div.append($quarter_div);
         }
         
